2ac92caf7f74d574394bcfb130250787faaceda0
Jake Hall <jake.hallslm@gmail.com>
2024-06-24 09:14:32 +0100

    remove dependency for db connection

--- src/lib/services/db.server.ts
+++ src/lib/services/db.server.ts
@@ -8,9 +8,12 @@ export const addMessageToDb = (
 	userId: string | null = null,
 	email: string | null = null
 ) => {
-	// return sql`INSERT INTO messages (assistant_id, thread_id, role, content, timestamp, user_id, email)
-	// VALUES (${ASSISTANT_ID}, 'thread_example_id', 'ROLE', 'This is an example message content.', '2024-05-11 12:00:00', 'username_example', 'example@example.com');`;
-	return sql`
+  // if no db vars set, just return
+  if (!process.env.POSTGRES_URL) {
+    console.log("No DB connections set.")
+    return
+  }	
+  return sql`
     INSERT INTO messages (assistant_id, thread_id, role, content, timestamp, user_id, email)
     VALUES (${ASSISTANT_ID}, 
     ${threadId}, ${role}, ${message}, 
