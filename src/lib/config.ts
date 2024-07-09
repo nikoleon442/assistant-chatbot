763655c8e51ece29eed152b8d3cd66f809b0f6bc
nikoleon442 <123130477+nikoleon442@users.noreply.github.com>
2024-07-09 10:13:39 +0100

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,11 +2,11 @@ export const MODEL = 'gpt-3.5-turbo';
 
 export const ENDPOINT = '/api/chat';
 
-export const AI_ASSISTANT = 'DiversifAI';
+export const AI_ASSISTANT = 'PayAsYouPrompt.com';
 
 export const AI_DESCRIPTION = 'Start your conversation';
 
-export const AI_AVATAR_PATH = '/Copy of PA YP (1).png';
+export const AI_AVATAR_PATH = '/Copy of PA YP (2).png';
 
 export const USER_AVATAR_PATH = '/17004.png';
 
